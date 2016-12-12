64d78fd111338d806f506bf104ea8aaad35aebb8
IIC <iic@IICs-MacBook-Pro.local>
2016-12-13 09:52:49 +1100

    bugfixes, comments

--- WatsonTranslation/ViewController.swift
+++ WatsonTranslation/ViewController.swift
@@ -13,25 +13,29 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
     var languageTranslationService : LanguageTranslator!
     var textToSpeechService : TextToSpeech!
     var audioPlayer : AVAudioPlayer!
     var selectedVoice : String!
     
-    // MARK: Props
-    // put in the watson services here. Text to speech and translation for now.
     @IBOutlet weak var toTranslateField: UITextField!
     
     // MARK: Actions
     @IBAction func translateButtonPressed(_ sender: UIButton) {
         let failure = { (error: Error) in print(error) }
         
+        // If the text field has text in it
         if let text = self.toTranslateField?.text {
+            // translate it to french
             self.languageTranslationService.translate(text, from: "en", to: "fr", failure: failure) { translation in
+                
+                // from the result, get the first provided translation, as there can be more than one provided
                 let firstTranslation = translation.translations[0].translation
                 
                 print(firstTranslation)
                 
+                // Say the translated text using the french voice
                 self.textToSpeechService.synthesize(firstTranslation, voice: "fr-FR_ReneeVoice", failure: failure) { data in
                    self.audioPlayer = try! AVAudioPlayer(data: data)
                    self.audioPlayer.prepareToPlay()
@@ -45,8 +49,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.white
-        self.textToSpeechService = TextToSpeech(username: "fcebe167-c753-4068-8e8c-e99c433c5621", password: "ARQRv0QYmY6e")
-        self.languageTranslationService = LanguageTranslator(username: "509d8694-5f9e-443e-99a8-7b397c0626cb", password: "Ep4LCnwvnFiF")
+        
+        // MARK: Insert credentials for bluemix Watson services here before using, otherwise the program won't work!
+        self.textToSpeechService = TextToSpeech(username: "", password: "")
+        self.languageTranslationService = LanguageTranslator(username: "", password: "")
         self.audioPlayer = AVAudioPlayer()
     }
 
